d375a38a5f949bbcbf008bad62ad291a77af21fa
blalshaheen224 <blalsheen2245852@gmail.com>
2026-07-04 18:32:11 +0300

    _ part _beatful ui

--- src/pages/admin/AdminProperties.jsx
+++ src/pages/admin/AdminProperties.jsx
@@ -67,7 +67,7 @@ const AdminProperties = () => {
         </Link>
       </div>
 
-      {/* Search Bar */}
+      {/* Search Bar
       <div className="bg-white rounded-xl shadow-sm p-4">
         <div className="relative">
           <Search className="absolute right-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
@@ -79,7 +79,7 @@ const AdminProperties = () => {
             className="w-full pr-10 pl-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary-500 focus:border-transparent"
           />
         </div>
-      </div>
+      </div> */}
 
       {/* Properties Table */}
       <div className="bg-white rounded-xl shadow-sm overflow-hidden">
